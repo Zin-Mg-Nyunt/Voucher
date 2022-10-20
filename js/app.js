a3292ba68d8adb44c7dcf4353b25e0a8d5078841
Zin Mg Nyunt <zinmgnyunt99@gmail.com>
2022-10-21 01:57:37 +0630

    finish state

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
 let items = document.querySelector("#items");
 let quantity = document.querySelector("#quantity");
 let inputForm = document.querySelector("#inputForm");
-
+let itemsList = document.querySelector("#itemsList");
+let totalCost = document.querySelector("#totalCost");
+let trash = document.querySelector("#trash");
 
 // process
 products.forEach((product)=>{
@@ -10,8 +12,40 @@ products.forEach((product)=>{
     items.append(newOption);
 })
 
+// totalCost
+function calcTotal() {
+    let cost = document.querySelectorAll(".cost");
+    let costs = [...cost].reduce((pv,cv)=>pv+Number(cv.innerText),0);
+    totalCost.innerText = costs;
+}
+
+// delete product
+function del() {
+    event.target.parentNode.parentNode.parentNode.remove();
+    calcTotal();
+}
+
 inputForm.addEventListener("submit",(e)=>{
     e.preventDefault();
-    console.log(items.value , quantity.value);
+
+    let currentItems = products.find(product=>product.id == items.value);
+    let itemCost = currentItems.price * Number(quantity.value);
+
+    let newTr = document.createElement("tr");
+    newTr.innerHTML = `
+        <td class="itemName">
+        ${currentItems.name}
+        <button class="btn btn-sm btn-danger" id="trash" onclick=del()>
+            <i class="bi bi-trash-fill"></i>
+        </button>
+        </td>
+        <td class="text-end">${currentItems.price}</td>
+        <td class="text-end">${quantity.value}</td>
+        <td class="text-end cost">${itemCost}</td>
+    `;
+    itemsList.append(newTr);
+    calcTotal();
+    
+    
     inputForm.reset();
 })
